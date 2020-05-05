fd697f1f88f9624135a1c883ebb3e20251b6f7a4
liniyakamnya <liniyakamnya@gmail.com>
2020-05-05 14:30:23 +0300

    Trading api explorer

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aws-lambda-trade-api
+This demo require AWS account
+Setup lambda function 
+Setup Cloud Watch event
+For testing purposes you can use string value in test event
+Execute gradlew build to create build
---
+++ src/main/java/com/demo/trade/TradeApiScanner.java
@@ -0,0 +1,77 @@
+package com.demo.trade;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TradeApiScanner implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String companyName = input.toString();
+        logger.log("Input: " + companyName + "\n");
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet getRequest = new HttpGet(
+                    "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?region=US&symbol=" + companyName);
+            getRequest.addHeader("x-rapidapi-host", System.getenv("x_rapidapi_host"));
+            getRequest.addHeader("x-rapidapi-key", System.getenv("x_rapidapi_key"));
+            CloseableHttpResponse response = httpClient.execute(getRequest);
+            String responseString = EntityUtils.toString(response.getEntity(), "utf-8");
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            Map<String, Object> responseMap = objectMapper.readValue(responseString, new TypeReference<Map<String, Object>>() {
+            });
+            BigDecimal currentPrice = new BigDecimal((String) ((Map<String, Object>) ((Map<String, Object>) responseMap.get("price"))
+                    .get("regularMarketPrice")).get("fmt"));
+            BigDecimal startPrice = new BigDecimal((String) ((Map<String, Object>) ((Map<String, Object>) responseMap.get("price"))
+                    .get("regularMarketOpen")).get("fmt"));
+            logger.log("currentPrice: " + currentPrice.toString() + "\n");
+            logger.log("startPrice: " + startPrice.toString() + "\n");
+
+            String phoneNumber = System.getenv("PHONE_NUMBER");
+            int compare = currentPrice.compareTo(startPrice);
+            AmazonSNSClient snsClient = new AmazonSNSClient();
+            if (compare > 0) {
+                String sellStocksMan = "Sell " + companyName + " stocks man";
+                sendSMSMessage(snsClient, sellStocksMan, phoneNumber, logger);
+                return sellStocksMan;
+            } else if (compare < 0) {
+                String buyStocksMan = "Buy " + companyName + " stocks man";
+                sendSMSMessage(snsClient, buyStocksMan, phoneNumber, logger);
+                return buyStocksMan;
+            }
+        } catch (IOException e) {
+            logger.log("Error during the call " + e.getMessage() + "\n");
+            throw new RuntimeException("Error during the api call", e);
+        }
+        return "Be patient";
+    }
+
+    public static void sendSMSMessage(AmazonSNSClient snsClient, String message,
+                                      String phoneNumber, LambdaLogger logger) {
+        logger.log(message + "\n");
+        PublishResult result = snsClient.publish(new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(phoneNumber)
+                .withMessageAttributes(new HashMap<>())
+        );
+        logger.log("Sent message ID:" + result.getMessageId() + "\n");
+    }
+}
